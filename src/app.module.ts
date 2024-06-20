e41a6af26ae64476915fb801e6be5ddff1c4ed0c
Ayoub Achak <ayoub.achak01@gmail.com>
2024-06-20 03:19:03 +0200

    missed this one

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,18 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ScheduleModule } from '@nestjs/schedule';
 import { TasksModule } from './tasks/tasks.module';
 import { AuthModule } from './auth/auth.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'test',
+      host: process.env.PG_HOST || 'localhost',
+      port: parseInt(process.env.PG_PORT) || 5432,
+      username: process.env.PG_USER || 'postgres',
+      password: process.env.PG_PASS || 'test',
       database: 'cron_simulator',
       autoLoadEntities: true,
       synchronize: true,
